d01a518e12e9829cd150d779bfb8847419c2827a
youssefmhamdabdh <youssef.abd.elfattah@outlook.com>
2026-05-30 18:42:51 +0300

    fix: resolve JVM target inconsistency and simplify CI to github flavor only

--- shots_studio/android/app/build.gradle.kts
+++ shots_studio/android/app/build.gradle.kts
@@ -23,12 +23,12 @@ android {
     compileOptions {
         // Flag to enable support for the new language APIs
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = "17"
     }
 
     buildFeatures {
