51047f94afc6cffaa8509aca66365589f354833b
youssefmhamdabdh <youssef.abd.elfattah@outlook.com>
2026-05-30 19:14:57 +0300

    fix: remove problematic afterEvaluate and use dynamic JVM target in CI

--- shots_studio/android/build.gradle.kts
+++ shots_studio/android/build.gradle.kts
@@ -16,24 +16,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    afterEvaluate {
-        if (project.plugins.hasPlugin("com.android.build.gradle")) {
-            (project.extensions.getByName("android") as com.android.build.gradle.BaseExtension).apply {
-                compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_17
-                    targetCompatibility = JavaVersion.VERSION_17
-                }
-            }
-        }
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
